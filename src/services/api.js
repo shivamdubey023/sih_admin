2aa8d3672dfd80faeed9f9f8ec37fd76f92759b9
kuro-shiv <dubeysk2003@gmail.com>
2026-03-28 00:02:38 +0530

    done

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'https://student-portal-backend-qs4a.onrender.com/'
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000'
 
 const api = axios.create({ 
   baseURL,
